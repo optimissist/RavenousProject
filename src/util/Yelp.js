28875c1e2229dc24ae46105e8de5f3fedcc194eb
Marla <marla.schevker@gmail.com>
2023-02-04 17:30:43 -0800

    Add files via upload
    final code for project

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = "X2jVCRMF5TORJ8TSKRNd-k1g4XtkEQ7n4o_dxSvPMPCsd29BR_oY6n8Um5SIkvmGQzoqLT9ISPvqS__--X2jbkG0NPkNj_-CWEYtFjeLsbOB3i4naHYfIc9DEww6Y3Yx";
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }));
+            }
+        });
+    }
+};
+
+
+export default Yelp;
